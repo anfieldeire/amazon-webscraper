7b2e3c55bcdbf9ea6aea3318e8c43f81181e791a
anfieldeire <gareth.hasson@gmail.com>
2024-02-01 17:03:49 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,4 +2,6 @@ import sqlite3
 
 INSERT_PRODUCTS = "INSERT INTO product_data VALUES(?,?,?,?,?,?);"
 
-PRICE_CHECK = ""
+PRICE_CHECK = """SELECT a.asin AS asin1, a.title AS title1, a.price AS price1, a.scrape_date as date1, b.asin AS asin2, b.title AS title2, b.price AS price2, b.scrape_date as date2,
+(a.price - b.price) AS difference from product_data a, product_data b
+WHERE a.price <> b.price AND b.scrape_date =:todays_date AND a.asin = b.asin AND b.price < a.price"""
